5b5fd70f152f500c7b896584e71a95a23dbee2ae
KVamshiKR <vamshikrishnareddykandukuri@outlook.com>
2025-05-17 18:16:42 +0530

    Made it More original, added banner images and own logos

--- script.js
+++ script.js
@@ -90,6 +90,14 @@ async function main() {
         currentsong.currentTime = ((currentsong.duration) * percent)/100;
       })
     
+
+      document.querySelector(".hamburger").addEventListener("click",  ()=>{
+        document.querySelector(".left").style.left = "0";
+      })
+
+      document.querySelector(".close").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = "-110%";
+      })
     
 } 
 
